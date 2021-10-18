0a25e47152f72063b67f3587a061dced73e3634c
Samuel <samuelmodestoes@gmail.com>
2021-10-18 18:12:37 -0300

    implemented configs for eureka clients

--- payroll/src/main/java/com/samuel/modesto/payroll/PayrollApplication.java
+++ payroll/src/main/java/com/samuel/modesto/payroll/PayrollApplication.java
@@ -2,10 +2,10 @@ package com.samuel.modesto.payroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@RibbonClient(name = "worker")
+@EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
 public class PayrollApplication {
